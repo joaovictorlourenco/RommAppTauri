cc81f158fe7ec7c6d615c8afd1a5aaef3233e5a9
João <jv232011@gmail.com>
2025-05-23 19:58:06 -0300

    "tada: primeiro commit"

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+function App() {
+  return (
+    <body className="bg-background-color min-h-screen flex items-center justify-center">
+      <div className="grid grid-cols-5 gap-4">
+        <div className="bg-white p-10 rounded shadow">Jogo 1</div>
+        <div className="bg-white p-10 rounded shadow">Jogo 2</div>
+        <div className="bg-white p-10 rounded shadow">Jogo 3</div>
+        <div className="bg-white p-10 rounded shadow">Jogo 4</div>
+        <div className="bg-white p-10 rounded shadow">Jogo 5</div>
+      </div>
+    </body>
+  );
+}
+
+export default App;
